07ac2b7f615aa0c6a5faf18a61bb7e61989e0ede
Jacob Moyer <jamoye@microsoft.com>
2018-10-10 15:58:30 -0700

    Add project files.

---
+++ TextRPG/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        /* Classes: Player, EnemyL1, EnemyL2, EnemyL3, EnemyL4, NPC, Menu, Main Menu, Attack, Dodge
+
+Player:
+Health - Property (if Health = 0 GAME OVER)
+Strength - Property
+Speed - Property
+Weapon - Property
+
+EnemyL1:
+Health - Property
+Strength - Property
+Speed - Property
+
+EnemyL2:
+Health - Property
+Strength - Property
+Speed - Property
+
+EnemyL3:
+Health - Property
+Strength - Property
+Speed - Property
+
+EnemyL4:
+Health - Property
+Strength - Property
+Speed - Property
+
+NPC:
+Buy - Method
+Sell - Method
+Balance - Balance
+
+Menu: 
+Save - Method
+Attack: Math to subtract strength from health - Method
+Weapon: Adds permanently to strength - Method
+Armor: Adds permanently to Health - Method
+Dodge: A percentage chance for an enemy attack to completely miss (generate random number and if within given range, attack is void)
+
+Main Menu:
+New Game
+Load Game
+Exit
+
+    Possible Example:
+A man lies in the road, what do you do?
+	1. Investigate
+	2. Go around
+
+Option 1
+The man is dead and a satchel of gold lies next to him. What do you do?
+	1. Investigate further
+	2. Pocket Gold
+	3. Check Surroundings
+	
+Option 2 
+You walk into the brush avoiding the man lying in the road, you hear voices in the distance. What do you do?
+	1. Go in the direction of the voices
+	2. Hide in a nearby bush
+	
+**Possibly have all options and their outcomes in an array/class which can be referenced by other classes**
+
+Main Menu
+	New Game
+	Load Game
+	Exit
+		Menu
+Story Unfolds
+*/
+        }
+    }
+}
